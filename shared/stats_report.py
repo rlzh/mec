f6fb9f9ed645fe59878e690377a2786ddbb00589
rlzh <ryan_lzh@hotmail.com>
2019-03-18 10:25:51 -0400

    Updated clean data set with latest generated. Added valence & arousal range to stats report

--- shared/stats_report.py
+++ shared/stats_report.py
@@ -16,26 +16,28 @@ def get_lyric_wordcount(csv_path):
                 s.add(w)
                 unique_count[i] += 1
     print("Lyric word count info - '{}'".format(csv_path))
-    print("min word count: {}, i = {}".format(
+    print("min word count: {} (id: {})".format(
         np.min(word_count), np.argmin(word_count)))
-    print("max word count: {}, i = {}".format(
+    print("max word count: {} (id: {})".format(
         np.max(word_count), np.argmax(word_count)))
     print("ave word count: {}".format(np.mean(word_count)))
     print()
     print("unique count")
-    print("min unique count: {}, i = {}".format(
+    print("min unique count: {} (id: {})".format(
         np.min(unique_count), np.argmin(unique_count)))
-    print("max unique count: {}, i = {}".format(
+    print("max unique count: {} (id: {})".format(
         np.max(unique_count), np.argmax(unique_count)))
     print("ave unique count: {}".format(np.mean(unique_count)))
     print()
     return word_count, unique_count
 
 
-def get_class_info(csv_path):
+def get_emotion_info(csv_path):
     df = pd.read_csv(csv_path)
     class_distrib = df['y'].value_counts()
-    print("Class Distrib. - '{}'".format(csv_path))
+    print("Emotion info - '{}'".format(csv_path))
+    print("valence range: [{},{}]".format(df.valence.min(), df.valence.max()))
+    print("arousal range: [{},{}]".format(df.arousal.min(), df.arousal.max()))
     print(class_distrib)
     return class_distrib
 
@@ -49,8 +51,8 @@ def print_song(df, i):
 
 
 spotify_wc, spotify_uc = get_lyric_wordcount(const.CLEAN_SPOTIFY)
-spotify_class_distrib = get_class_info(const.CLEAN_SPOTIFY)
+spotify_class_distrib = get_emotion_info(const.CLEAN_SPOTIFY)
 
 
 deezer_wc, deezer_uc = get_lyric_wordcount(const.CLEAN_DEEZER)
-deezer_class_distrib = get_class_info(const.CLEAN_DEEZER)
+deezer_class_distrib = get_emotion_info(const.CLEAN_DEEZER)
